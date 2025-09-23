b12767f95fdb0a0295617ffabd445ab2d1e1209f
AdmirJr <admirjunior55@gmail.com>
2025-09-23 16:12:15 -0300

    documentation hotfix

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1060,7 +1060,6 @@ check_args <- function(args){
 #' @param n corresponds to the \link{colorRampPalette} parameter 
 #' @return returns a \link{colorRampPalette} function with the gen3sis richness colors
 #' @export
-#' @noRd
 color_richness_CVDCBP <- colorRampPalette(
   c("#B2F2FD", "#81EEEA", "#61E5C9", "#63DAA0", "#73CE79", "#85BF51", "#94AD2F", "#9B951B",
     "#9C7E1F", "#9A692B", "#985538", "#944444", "#933251", "#901F61", "#8C0172")
@@ -1071,7 +1070,6 @@ color_richness_CVDCBP <- colorRampPalette(
 #' @param n corresponds to the \link{colorRampPalette} parameter 
 #' @return returns a \link{colorRampPalette} function with the gen3sis richness colors
 #' @export
-#' @noRd
 color_richness <- colorRampPalette(
   c("#440154FF", "#482878FF", "#3E4A89FF", "#31688EFF", "#26828EFF", "#1F9E89FF", "#35B779FF",
     "#6DCD59FF", "#B4DE2CFF", "#FDE725FF", "#FFA500",   "#FF2900",   "#C40000",   "#8B0000", "#8B0000")

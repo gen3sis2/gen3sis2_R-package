162da90aeaa665a9c88f5ce036c3b5d1fc5ca38b
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2026-07-21 16:00:11 +1200

    purge branch

--- inst/examples/write_config_skeleton_help.R
+++
@@ -1,4 +0,0 @@
-# set config_empty.R file path
-config_file_path <- file.path(tempdir(), "config_empty.R")
-#writes out a config skeleton
-write_config_skeleton(config_file_path)
--- tests/testthat.R
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2020, ETH Zurich
-
-library(testthat)
-library(gen3sis2)
-
-test_check("gen3sis2")
